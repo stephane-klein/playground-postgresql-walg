6ba585b5d8b11ee84f1bdad1cc4e6da27a4429d4
Stéphane Klein <contact@stephane-klein.info>
2020-04-01 23:08:04 +0200

    Remove alpine and debian folder, keep only alpine version

--- debian/scripts/restore-pg2.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-cd "$(dirname "$0")/../"
-
-docker-compose stop postgres2
-docker-compose run --rm postgres2 sh -c '/wal-g backup-fetch $PGDATA LATEST; touch $PGDATA/recovery.signal'
-docker-compose up -d postgres2
--- debian/scripts/teardown.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-cd "$(dirname "$0")/../"
-
-docker-compose down --rmi all
-rm -rf volumes
